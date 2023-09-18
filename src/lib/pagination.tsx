ae7f1dbd63f009f19a28818c0cdc611f7dd24205
Nathanael <Nathan@MacBook-Pro.local>
2023-09-18 09:50:14 +0700

    Fix file name

--- src/lib/pagination.tsx
+++ src/lib/pagination.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-export default function handlePagination() {
+export default function HandlePagination() {
   const [currentPage, setCurrentPage] = useState<number>(1);
   const itemsPerPage = 4;
   const indexOfLastItem = currentPage * itemsPerPage;
